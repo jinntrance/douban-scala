fe64a112592a204185a786d80a257315d08dc45a
Joseph <jinntrance@gmail.com>
2013-01-10 19:58:42 +0800

    Book testing fulfilled after refactoring

--- src/main/scala/com/douban/models/Bean.scala
+++ src/main/scala/com/douban/models/Bean.scala
@@ -36,7 +36,7 @@ trait Flatten {
    *
    * @return 把Bean转化为key=value&key1=value1的序列 ,添加apikey
    */
-  def toParas: String =Auth.addApiKey+flat(decompose(this))
+  def toParas: String = Auth.addApiKey + flat(decompose(this))
 
   /**
    * 层级参数全部flattened 成一层的key-value形式，
@@ -60,16 +60,19 @@ trait Flatten {
     }
     para
   }
-  private def encode(value:String)=URLEncoder.encode(value,"utf-8")
+
+  private def encode(value: String) = URLEncoder.encode(value, "utf-8")
 }
 
 class Bean extends Flatten {
 }
 
-abstract class API[+T:Manifest,+R:Manifest] {
+abstract class API[+T: Manifest, +R: Manifest] {
   var secured = false
   val api_prefix: String = "https://api.douban.com/v2/"
-  def url_prefix:String
+
+  def url_prefix: String
+
   private val byIdUrl = url_prefix + "%s"
   private val searchUrl = url_prefix + "search"
 
@@ -77,16 +80,19 @@ abstract class API[+T:Manifest,+R:Manifest] {
    * 通过id获取
    */
   def byId(id: String) = get[T](byIdUrl.format(id))
+
   /**
    * 搜索
    */
-  def search(query: String, page: Int = 0, count: Int = 20, tag: String="") = get[R](new Search(query, page * count, count,tag).flatten(searchUrl))
+  def search(query: String, page: Int = 0, count: Int = 20, tag: String = "") = get[R](new Search(query, page * count, count, tag).flatten(searchUrl))
 }
-abstract class BookMovieMusicAPI[+T:Manifest,+R:Manifest] extends API[T,R]{
+
+abstract class BookMovieMusicAPI[+T: Manifest, +R: Manifest] extends API[T, R] {
   private val popTagsUrl = url_prefix + "%s/tags"
   private val reviewsPostUrl = url_prefix + "reviews"
   private val reviewUpdateUrl = url_prefix + "review/%s"
-  private val tagsUrl = url_prefix + "user/user_tags/%s"
+  private val tagsUrl = url_prefix + "user_tags/%s"
+
   /**
    * 获取某个Item中标记最多的标签
    */
@@ -95,7 +101,7 @@ abstract class BookMovieMusicAPI[+T:Manifest,+R:Manifest] extends API[T,R]{
   /**
    * 发表新评论
    */
-  def postReview[R<:ReviewPosted](r: ReviewPosted): Boolean = postNoResult(reviewsPostUrl, r)
+  def postReview[R <: ReviewPosted](r: R): Boolean = postNoResult(reviewsPostUrl, r)
 
   /**
    * 修改评论
@@ -114,6 +120,7 @@ abstract class BookMovieMusicAPI[+T:Manifest,+R:Manifest] extends API[T,R]{
   def tags(userId: String) = get[TagsResult](tagsUrl.format(userId))
 
 }
+
 /**
  * 标签信息
  */
@@ -126,7 +133,7 @@ case class Tag(count: Int, name: String)
  * @param count 返回总数
  * @param tags  图书 电影可以传tags
  */
-class Search(q: String, start: Int=0, count: Int=20,tags:String="") extends Bean
+case class Search(q: String, start: Int = 0, count: Int = 20, tags: String = "") extends Bean
 
 case class Rating(max: Int, min: Int, value: String)
 
@@ -140,6 +147,7 @@ case class RatingDetail(max: Int, min: Int, average: String, numRaters: Int)
 class ListResult(start: Int, count: Int, total: Int)
 
 class Review(id: Long, title: String, alt: String, author: User, rating: Rating,
-                  votes: Int, useless: Int, comments: Int, summary: String, published: Date, updated: Date)
+             votes: Int, useless: Int, comments: Int, summary: String, published: Date, updated: Date)
+
 class ReviewPosted(title: String, content: String, rating: Int = 0) extends Bean
 
