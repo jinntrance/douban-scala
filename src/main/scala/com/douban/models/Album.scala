465b6f7e31d6f3f73ece551e13c6bfd285b488e8
Joseph <jinntrance@gmail.com>
2013-01-17 18:06:12 +0800

    添加album

---
+++ src/main/scala/com/douban/models/Album.scala
@@ -0,0 +1,33 @@
+package com.douban.models
+import java.util.Date
+import com.douban.common.Req._
+
+/**
+ * Copyright by <a href="http://crazyadam.net"><em><i>Joseph J.C. Tang</i></em></a> <br/>
+ * Email: <a href="mailto:jinntrance@gmail.com">jinntrance@gmail.com</a>
+ * @author joseph
+ * @since 1/17/13 5:26 PM
+ * @version 1.0
+ * @see http://developers.douban.com/wiki/?title=api_v2
+ */
+ 
+ object Album extends API[Album]{
+  protected def url_prefix: String = api_prefix+"album/"
+  private val photosUrl=idUrl+"/photos"
+  def photos(s:AlbumSearch=new AlbumSearch)=get[AlbumsResult](s.flatten(photosUrl))
+}
+
+object Photo extends API[Photo] with Comment[Photo]{
+  protected def url_prefix: String = api_prefix+"photo"
+}
+case class Size(icon:List[Int],thumb:List[Int],cover:List[Int],image:List[Int])
+case class Photo(id:String,alt:String,album_id:String,album_title:String,icon:String,thumb:String,cover:String,image:String,desc:String,created:Date,privacy:String,position:Int,prev_photo:String,next_photo:String,liked_count:Int,recs_count:Int,author:User,liked:Boolean,sizes:Size)
+case class Album(id:String,alt:String,title:String,icon:String,cover:String,thumb:String,image:String,privacy:String,created:Date,updated:Date,liked_count:Int,recs_count:Int,size:Int,desc:String,author:User,liked:Boolean,sizes:Size)
+
+/**
+ * @param order 排序	asc, desc, 默认为相册本身的排序
+ * @param sortby 排序方式	time 上传时间，vote 推荐数，comment 回复数，默认为time
+ */
+case class AlbumSearch(start:Int=0,count:Int=20,order:String="",sortby:String="time") extends Bean
+case class AlbumsResult(start:Int,count:Int,total:Int,photos:List[Photo]) extends ListResult(start,count,total)
+
